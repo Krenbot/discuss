49e9cd9da239543cfbe3268ac6cfc158b6e9ac5b
Steven Krenn <stevekrenn@gmail.com>
2024-09-04 19:41:32 -0500

    add name/description input client error handling

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,40 @@
 'use server';
+import { z } from 'zod';
 
-export async function createTopic() {
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-zA-Z- ]+$/, {
+      message:
+        'Name can include uppercase and lowercase letters, dashes, and spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return { errors: {} };
+
   //TODO: Revalidate the homepage after creating a topic
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Input,
   Button,
@@ -6,24 +8,36 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from '@nextui-org/react';
-
+import { useFormState } from 'react-dom';
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, { errors: {} });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create A Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form onSubmit={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="name" labelPlacement="outside" placeholder="name" />
+            <Input
+              name="name"
+              label="name"
+              labelPlacement="outside"
+              placeholder="name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
             <Textarea
+              name="description"
               label="description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type="submit">Submit</Button>
           </div>
