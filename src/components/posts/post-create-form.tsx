7d39bf2d5331d7cc4cedea9aba2c35122cb66263
Steven Krenn <stevekrenn@gmail.com>
2024-09-06 09:02:36 -0500

    display post creation form, apply validation

---
+++ src/components/posts/post-create-form.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
+import FormButton from '@/components/common/form-button';
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@nextui-org/react';
+
+export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  });
+
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create A Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+            <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+            <Textarea
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+            <FormButton>Create post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
