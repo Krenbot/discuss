6bbf30a6dc14ceced26b67761fb39b93e53959e9
Steven Krenn <stevekrenn@gmail.com>
2024-09-22 16:39:29 -0500

    apply grayscale skeleton component to show instead of loading text

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
-import { Suspense } from "react";
-import PostShow from "@/components/posts/post-show";
-import CommentList from "@/components/comments/comment-list";
-import CommentCreateForm from "@/components/comments/comment-create-form";
-import paths from "@/paths";
-import { fetchCommentsByPostId } from "@/db/queries/comments";
+import Link from 'next/link';
+import { Suspense } from 'react';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
+import PostShowLoading from '@/components/posts/post-show-loading';
 
 interface PostShowPageProps {
   params: {
@@ -19,9 +20,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {"< "}Back to {slug}
+        {'< '}Back to {slug}
       </Link>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<PostShowLoading />}>
         <PostShow postId={postId} />
       </Suspense>
       <CommentCreateForm postId={postId} startOpen />
